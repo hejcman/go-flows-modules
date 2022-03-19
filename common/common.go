29dacf5955d87f895b1b236944ee9f3607425001
Lukáš Hejcman <xhejcm01@vutbr.cz>
2022-03-19 08:33:17 +0100

    Added Biflows PEN.

--- common/common.go
+++ common/common.go
@@ -10,4 +10,7 @@ var (
 	// CesnetPen is the pen of CESNET, as defined by RFC 7013
 	// https://datatracker.ietf.org/doc/html/draft-ietf-ipfix-ie-doctors#section-10.1
 	CesnetPen uint32 = 8057
+
+	// BiflowsPen as defined here: https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers
+	BiflowsPen uint32 = 29305
 )
